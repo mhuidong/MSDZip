3ed4dbeb7405a176741e60926c806d6f12c4b378
Ma Huidong <mahd@nbjl.nankai.edu.cn>
2024-10-18 20:59:26 +0800

    Add files via upload

--- sp-compress.sh
+++ sp-compress.sh
@@ -2,7 +2,8 @@
 #source activate pt2
 ulimit -n 100000
 data=$1
-prefix=$2
+output=$2
+prefix=$3
 
 WATCH_DIR=${prefix}_model
 FileBaseName=$(basename ${data})   # data name
@@ -18,7 +19,7 @@ python split_data.py ${FileBaseName} -n 2
 
 pids=()
 if [ -z "$(ls -A "$WATCH_DIR")" ]; then
-    python compress.py ${FileBaseName}.0 ${FileBaseName}.0.msdzip --save -i 0 --prefix ${prefix} --gpu 0 &
+    python compress.py ${FileBaseName}.0 ${FileBaseName}.0.mz --save -i 0 --prefix ${prefix} --gpu 0 &
     pids+=($!)
 fi
 
@@ -26,7 +27,7 @@ fi
 for i in 1; do
     inotifywait -e create --format '%f' "$WATCH_DIR" | while read file; do
         if [ "$file" == ${prefix}".$((i - 1)).pth" ]; then  #"model.$((i - 1)).pth"
-            python compress.py ${FileBaseName}.${i} ${FileBaseName}.${i}.msdzip --load -i ${i} --prefix ${prefix} --gpu 1
+            python compress.py ${FileBaseName}.${i} ${FileBaseName}.${i}.mz --load -i ${i} --prefix ${prefix} --gpu 1
         fi
     done
 done
@@ -35,11 +36,11 @@ for pid in "${pids[@]}"; do
     wait $pid
 done
 
-tar -czf ${FileBaseName}.msdzip ${FileBaseName}.0.msdzip ${FileBaseName}.1.msdzip
+tar -czf ${output} ${FileBaseName}.0.mz ${FileBaseName}.1.mz
 
 rm -rf ${WATCH_DIR}
 rm -rf ${FileBaseName}.0
 rm -rf ${FileBaseName}.1
-rm -rf ${FileBaseName}.0.msdzip
-rm -rf ${FileBaseName}.1.msdzip
+rm -rf ${FileBaseName}.0.mz
+rm -rf ${FileBaseName}.1.mz
 
--- sp-decompress.sh
+++ sp-decompress.sh
@@ -2,7 +2,8 @@
 #source activate pt2
 ulimit -n 100000
 data=$1
-prefix=$2
+output=$2
+prefix=$3
 
 WATCH_DIR=${prefix}_model
 FileBaseName=$(basename ${data})   # data name
@@ -14,19 +15,19 @@ if [ -d "$WATCH_DIR" ]; then
 fi
 mkdir -p "$WATCH_DIR"
 
-tar -xvf ${FileBaseName}.msdzip
+tar -xvf ${FileBaseName}.mz
 
 pids=()
 START_TIME=$(date +%s)
 if [ -z "$(ls -A "$WATCH_DIR")" ]; then
-    python decompress.py ${FileBaseName}.0.msdzip ${FileBaseName}.0.out --save -i 0 --prefix ${prefix} --gpu 0 & # 并行执行 X 脚本 >
+    python decompress.py ${FileBaseName}.0.mz ${FileBaseName}.0.out --save -i 0 --prefix ${prefix} --gpu 0 & # 并行执行 X 脚本 >
     pids+=($!)
 fi
 #
 for i in 1; do
     inotifywait -e create --format '%f' "$WATCH_DIR" | while read file; do
         if [ "$file" == ${prefix}".$((i - 1)).pth" ]; then  #"model.$((i - 1)).pth"
-            python decompress.py ${FileBaseName}.${i}.msdzip ${FileBaseName}.${i}.out --load -i ${i} --prefix ${prefix} --gpu 1
+            python decompress.py ${FileBaseName}.${i}.mz ${FileBaseName}.${i}.out --load -i ${i} --prefix ${prefix} --gpu 1
         fi
     done
 done
@@ -35,10 +36,10 @@ for pid in "${pids[@]}"; do
 done
 
 
-cat ${FileBaseName}.0.out ${FileBaseName}.1.out > ${FileBaseName}.out
+cat ${FileBaseName}.0.out ${FileBaseName}.1.out > ${output}
 
 rm -rf "$WATCH_DIR"
-rm -rf ${FileBaseName}.0.msdzip
-rm -rf ${FileBaseName}.1.msdzip
+rm -rf ${FileBaseName}.0.mz
+rm -rf ${FileBaseName}.1.mz
 rm -rf ${FileBaseName}.0.out
 rm -rf ${FileBaseName}.1.out
